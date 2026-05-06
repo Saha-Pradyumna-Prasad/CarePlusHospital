f421763dfd9cb9974be6151267b480afe58cceef
Saha Pradyumna Prasad <bro28835@gmail.com>
2026-05-06 21:37:09 +0600

    Add files via upload

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "Running composer..."
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Creating storage link..."
+php artisan storage:link
+
+echo "Running seeders (for initial data)..."
+# php artisan db:seed --force
